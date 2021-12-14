aa84492819714dfff693c92a8c1e76ddbac8cfd0
gkrishnans <gokulakrishnanaji@gmail.com>
2021-12-14 15:30:56 +0530

    added config file

--- config.php
+++ config.php
@@ -1,15 +1,22 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'mysql');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_NAME', 'demo');
+include_once('config.inc.php');
+
+// define('DB_SERVER', 'mysql');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', 'root');
+// define('DB_NAME', 'demo');
+
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+// $link = mysqli_connect($dbusername, $dbusername, $dbpassword, "demo");
+$link = mysqli_connect($dbusername, $dbusername, $dbpassword, "demo");
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+else{
+    echo "connected successfully"
 }
 ?>
